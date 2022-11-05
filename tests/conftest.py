070dbc9309c22abef838cfc0680b0518a462dcfd
Matheus Fillipe <matheusfillipeag@gmail.com>
2022-11-05 01:29:46 -0300

    Fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,18 @@ from gstui.gsclient import ThreadedCachedClient, CachedClient
 import pytest
 
 
+class MockedCachedClient(CachedClient):
+    def __init__(self):
+        pass
+
+
 @pytest.fixture(scope="function")
 def cache_path():
     with TemporaryDirectory() as tmpdir:
         yield tmpdir
 
+
 @pytest.fixture(scope="function")
 def cached_client(cache_path):
     ThreadedCachedClient.cache_path = cache_path
-    return CachedClient()
+    return MockedCachedClient()
